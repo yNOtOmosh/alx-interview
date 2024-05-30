cd5295ff869923c6343a2513b805ab3cfa1a4a25
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-05-30 10:03:58 -0700

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,44 +3,62 @@
 import sys
 
 
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]."""
+    # Check the column
+    for i in range(row):
+        if board[i] == col:
+            return False
+
+    # Check the upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i] == j:
+            return False
+
+    # Check the upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, len(board), 1)):
+        if board[i] == j:
+            return False
+
+    return True
+
+def solve_nqueens(board, row, solutions):
+    """Solve the N queens problem using backtracking."""
+    if row == len(board):
+        solutions.append(board[:])
+        return
+
+    for col in range(len(board)):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(board, row + 1, solutions)
+            board[row] = -1
+
+def print_solutions(solutions):
+    """Print the solutions in the required format."""
+    for solution in solutions:
+        formatted_solution = [[i, solution[i]] for i in range(len(solution))]
+        print(formatted_solution)
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     try:
-        n = int(sys.argv[1])
+        N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    if n < 4:
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
 
+    board = [-1] * N
     solutions = []
-    backtrack(n, 0, [], solutions)
-
-    for solution in solutions:
-        print(solution)
-
-def backtrack(n, row, current_solution, solutions):
-    if row == n:
-        solutions.append(current_solution.copy())
-        return
-
-    for col in range(n):
-        if is_safe(current_solution, row, col):
-            current_solution.append(col)
-            backtrack(n, row + 1, current_solution, solutions)
-            current_solution.pop()
-
-def is_safe(current_solution, row, col):
-    for i in range(len(current_solution)):
-        if current_solution[i] == col or abs(current_solution[i] - col) == abs(i - row):
-            return False
-
-    return True
+    solve_nqueens(board, 0, solutions)
+    print_solutions(solutions)
 
 if __name__ == "__main__":
     main()

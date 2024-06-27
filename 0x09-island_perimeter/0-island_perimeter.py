4ea6b80c6f44100bfe2dd737902811ca3b30f0b5
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-06-27 12:16:40 -0700

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Island Perimeter"""
 
+
 def island_perimeter(grid):
     """Initialize perimeter count"""
     perimeter = 0
@@ -11,14 +12,13 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                """Check all four directions (up, down, left, right)"""
-                if r == 0 or grid[r-1][c] == 0: # Up
+                if r == 0 or grid[r-1][c] == 0:
                     perimeter += 1
-                if r == rows-1 or grid[r+1][c] == 0: # Down
+                if r == rows-1 or grid[r+1][c] == 0:
                     perimeter += 1
-                if c == 0 or grid[r][c-1] == 0: # Left
+                if c == 0 or grid[r][c-1] == 0:
                     perimeter += 1
-                if c == cols-1 or grid[r][c+1] == 0: # Right
+                if c == cols-1 or grid[r][c+1] == 0:
                     perimeter += 1
 
     return perimeter
